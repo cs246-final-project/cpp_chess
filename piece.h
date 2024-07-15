4877850328d884d4731d582d76d911e0f2b1cd14
Hyunbeen <treblabin@gmail.com>
2024-07-15 14:39:16 -0400

    Add functions to piece.h

--- piece.h
+++ piece.h
@@ -1,10 +1,18 @@
 #ifndef PIECE_H
 #define PIECE_H
 
+#include <vector>
+
 class Piece {
+  int point;
+  bool isWhite;
 public:
   Piece();
-  ~Piece();
+  bool getIsWhite();
+  int getPoint();
+  virtual ~Piece() = 0;
+  virtual std::vector<std::vector<int>> getPossibleMoves(std::vector<int> current) = 0;
+  virtual bool isMovePossible(std::vector<int> current, std::vector<int> to) = 0;
 };
 
 #endif
