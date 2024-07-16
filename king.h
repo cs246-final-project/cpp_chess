3e65607d56880d014ab1cf5dca38f15b1e2ac929
Hyunbeen <treblabin@gmail.com>
2024-07-16 18:41:45 -0400

    Implement King (Castling not implemented)

--- king.h
+++ king.h
@@ -3,7 +3,13 @@
 #include "piece.h"
 
 class King : public Piece {
-  bool castlingPossible;
+  bool didFirstMove;
+  public:
+    King(bool isWhite);
+    bool isMoveLegal(vector<int> current, vector<int> destination, Board &board) override;
+    vector<vector<int>> getLegalMoves(vector<int> current, Board &board) override;
+    void setDidFirstMove();
+    bool getDidFirstMove();
 };
 
 #endif
