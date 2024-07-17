f72d400514dab8693305a85076451bf8cefca1b4
Hyunbeen <treblabin@gmail.com>
2024-07-17 16:31:27 -0400

    Implement Knight

--- knight.h
+++ knight.h
@@ -3,7 +3,10 @@
 #include "piece.h"
 
 class Knight : public Piece {
-
+  public:
+    Knight(const bool isWhite);
+    bool isMoveLegal(vector<int> current, vector<int> destination, Board &board) override;
+    vector<vector<int>> getLegalMoves(vector<int> current, Board &board) override;
 };
 
 #endif
